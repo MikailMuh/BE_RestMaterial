b9a623aed47693c271442885f1529fe4c2964e6d
MikailMuh <mikail.2018.7@gmail.com>
2026-04-30 09:27:57 +0700

    deployment fix

--- src/server.js
+++ src/server.js
@@ -17,7 +17,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const needPush;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
